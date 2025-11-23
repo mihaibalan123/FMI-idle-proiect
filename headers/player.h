7fc059fcff8abde73c37ad006a9f955f82703100
mihai <mihaibalan568@gmail.com>
2025-11-24 00:27:56 +0200

    am mai facut private functiile din player.h

--- headers/player.h
+++ headers/player.h
@@ -18,6 +18,16 @@ class player {
     std::vector<int> project_levels;
     std::vector<int> defeated_domains;
     long long last_login_timestamp = 0;
+    void add_project_id(int id);
+    void add_defeated_domain(int domain_id);
+    void calculate_and_set_conquer_domain();
+    void player_stats(std::ostream& os) const;
+    void reset_projects();
+    void player_full_stats(std::ostream& os, const std::vector<project>& projects_list) const;
+    void reset_game();
+    void fight_teacher(const teacher& opponent, int p_project_id);
+    void project_upgrade(int selected_id, const std::vector<project>& projects_list);
+    [[nodiscard]] int turns_to_defeat(float enemy_hp) const;
 public:
     player(std::string name, std::string password, int conquer_domain, float currency1, float currency2, float health, float damage, std::vector<int> project_id,std::vector<int> project_levels, std::vector<int> defeated_domains = {}, long long last_login_timestamp  = 0): username(std::move(name)),password(std::move(password)), current_target_domain_id(conquer_domain), currency1(currency1), currency2(currency2), health(health), damage(damage), project_id(std::move(project_id)),project_levels(std::move(project_levels)), defeated_domains(std::move(defeated_domains)), last_login_timestamp(last_login_timestamp) {};
     player() : current_target_domain_id(0), currency1(0.0f), currency2(0.0f), health(0.0f), damage(0.0f), last_login_timestamp(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count()) {}
@@ -28,13 +38,6 @@ public:
     void reset_progress();
     void enter_examination_room(const std::vector<teacher>& teachers_list);
     void perform_upgrade(const std::vector<project>& projects_list);
-
-    void add_project_id(int id);
-    void add_defeated_domain(int domain_id);
-    void calculate_and_set_conquer_domain();
-    [[nodiscard]] int turns_to_defeat(float enemy_hp) const;
-    void project_upgrade(int selected_id, const std::vector<project>& projects_list);
-    void fight_teacher(const teacher& opponent, int p_project_id);
 
     [[nodiscard]] const std::string& get_password() const;
     [[nodiscard]] const std::vector<int>& get_project_id() const;
@@ -56,11 +59,7 @@ public:
     static void save_players(const std::vector<player>& players_list);
     static int add_new_player(std::vector<player>& players_list);
     [[nodiscard]] bool verify_password() const;
-    void reset_projects();
-    void player_stats(std::ostream& os) const;
-    void player_full_stats(std::ostream& os, const std::vector<project>& projects_list) const;
     void idle_earnings(const std::vector<project>& projects_list);
-    void reset_game();
 
     friend std::istream& operator>>(std::istream& is, player &t);
     friend std::ostream& operator<<(std::ostream& os, const player& t);
